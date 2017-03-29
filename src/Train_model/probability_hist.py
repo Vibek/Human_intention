d268432e96cae895d81797f5bd0aa9df56605033
Vibekananda Dutta <vibek4989@gmail.com>
2017-03-29 12:01:43 +0200

    Update results

--- src/Train_model/probability_hist.py
+++ src/Train_model/probability_hist.py
@@ -8,24 +8,25 @@ plt.close('all')
 plt.figure(1)
 
 #read the data
-data_D = np.genfromtxt('/home/vibek/Human_intention/src/Train_model/Afforance_Probability/data_D.csv', delimiter=',')
-data_P = np.genfromtxt('/home/vibek/Human_intention/src/Train_model/Afforance_Probability/data_P.csv', delimiter=',')
-data_Pa = np.genfromtxt('/home/vibek/Human_intention/src/Train_model/Afforance_Probability/data_Pa.csv', delimiter=',')
-data_M = np.genfromtxt('/home/vibek/Human_intention/src/Train_model/Afforance_Probability/data_M.csv', delimiter=',')
+data_G_ch = np.genfromtxt('/home/vibek/Human_intention/src/Train_model/Afforance_Probability/data_G_ch.csv', delimiter=',')
+data_R_ch = np.genfromtxt('/home/vibek/Human_intention/src/Train_model/Afforance_Probability/data_R_ch.csv', delimiter=',')
+data_P_ch = np.genfromtxt('/home/vibek/Human_intention/src/Train_model/Afforance_Probability/data_P_ch.csv', delimiter=',')
+#data_M = np.genfromtxt('/home/vibek/Human_intention/src/Train_model/Afforance_Probability/data_M.csv', delimiter=',')
 # use the number of bins (means Frequecy)
 numbins=12
 
 #remove all the NAN values in the data-set
-data_D =  data_D[~np.isnan(data_D)]
-data_P =  data_P[~np.isnan(data_P)]
-data_Pa = data_Pa[~np.isnan(data_Pa)]
-data_M =  data_M[~np.isnan(data_M)]
+data_G_ch =  data_G_ch[~np.isnan(data_G_ch)]
+data_R_ch =  data_R_ch[~np.isnan(data_R_ch)]
+data_P_ch =  data_P_ch[~np.isnan(data_P_ch)]
+#data_M =  data_M[~np.isnan(data_M)]
 
 #plot the histogram of each data-set in one plot
-plt.hist([data_D, data_P, data_Pa, data_M], numbins, normed=True, color=['red','green','blue', 'deeppink'], alpha=1, label=['Drinkable','Pourable','Placeable', 'Moveable'])
-plt.legend(loc='upper left' )
-plt.title('Probability Graph of Affrodances (Drinking Water from a Bottle) ', size=15)
-plt.xlabel("Probability value (0-1)", size=12)
-plt.ylabel("Frequency", size=12)
+plt.hist([data_G_ch, data_R_ch, data_P_ch], numbins, normed = False, color=['red','green','blue'], alpha=1, label=['Reachable','Graspable','Pullable'], orientation='horizontal')
+#plt.set_ylim([0, 1])
+plt.legend(loc='lower right' )
+plt.title('Probability Graph of Affrodances (Pulling a Chair) ', size=15)
+plt.xlabel("Frequency", size=12)
+plt.ylabel("Probability values (0-1)", size=12)
 plt.tick_params(labelsize=10)
 plt.show()

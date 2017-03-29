45ea59a99b710dce2bcf4ba3035e5fa710d2d224
Vibekananda Dutta <vibek4989@gmail.com>
2017-03-29 15:33:21 +0200

    update few files

--- src/Train_model/prediction_plot.py
+++ src/Train_model/prediction_plot.py
@@ -3,10 +3,8 @@
 from mpl_toolkits.mplot3d import axes3d
 import matplotlib.pyplot as plt
 import numpy as np
-#from matplotlib.mlab import griddata
 from scipy.interpolate import griddata
 from matplotlib import cm
-#from matplotlib.ticker import LinearLocator, FormatStrFormatter
 from numpy import genfromtxt
 
   
@@ -34,11 +32,17 @@ x2 = data2[:,0]
 y2 = data2[:,1]
 z2 = data2[:,2]
    
-xi = np.linspace(x2.min(),x2.max())
-yi = np.linspace(y2.min(),y2.max())
+
+data1 = np.genfromtxt('/home/vibek/Human_intention/src/Train_model/Potential_map/predict_drink_contour.csv', delimiter=',')
+x1 = data1[:,0]
+y1 = data1[:,1]
+z1 = data1[:,2]
+   
+xi = np.linspace(x1.min(),x1.max())
+yi = np.linspace(y1.min(),y1.max())
 
 # VERY IMPORTANT, to tell matplotlib how is your data organized
-zi = griddata((x2, y2), z2, (xi[None,:], yi[:,None]), method='cubic')
+zi = griddata((x1, y1), z1, (xi[None,:], yi[:,None]), method='cubic')
 
 ii = -1 #index of variable or spot point
 xm = 0.01 ##boxe size left corner
@@ -50,33 +54,30 @@ zp = 0.03
 bd = 5
 bs = 0.1
 
-xx = np.linspace(x[ii] - xm, x[ii] + xp, bd)
-yy = np.linspace(y[ii] - ym, y[ii] + yp, bd)
-zz = np.linspace(z[ii] - zm, z[ii] + zp, bd)
+#xx = np.linspace(x1[ii] - xm, x1[ii] + xp, bd)
+#yy = np.linspace(y1[ii] - ym, y1[ii] + yp, bd)
+#zz = np.linspace(z1[ii] - zm, z1[ii] + zp, bd)
    
-#xx = np.linspace(x[ii] - bs, x[ii] + bs, bd)
-#yy = np.linspace(y[ii] - bs, y[ii] + bs, bd)
-#zz = np.linspace(z[ii] - bs, z[ii] + bs, bd)
+xx = np.linspace(x1[ii] - bs, x1[ii] + bs, bd)
+yy = np.linspace(y1[ii] - bs, y1[ii] + bs, bd)
+zz = np.linspace(z1[ii] - bs, z1[ii] + bs, bd)
 
 # Make the grid    
-X,Y,Z = np.meshgrid(xx, yy, zz)
+#X, Y, Z = np.meshgrid(xx, yy, zz)
 
-u = -(X - x[ii])
-v = -(Y - y[ii])
-w = -(Z - z[ii])
+#u = -(X - x1[ii])
+#v = -(Y - y1[ii])
+#w = -(Z - z1[ii])
    
-#ax.scatter(x2, z2, y2, c='g', marker='*', label = 'Heat-map')
-ax.scatter(x, y, z, c='r', marker='o')
-ax.plot(x, y, z, c='k', linewidth = 2,label='Predicted trajectory')
-ax1.scatter(x, y, z, c='b', marker='+')
-ax1.plot(x2, y2, z2, c='r', linewidth = 1.5, label='right hand trajectory')
-ax3.plot(x3, y3, z3, c='g', label='left hand trajectory')
+ax.plot(x,   y,  z,  c='r', linewidth = 3,   label='Predicted trajectory')
+ax1.plot(x2, y2, z2, c='K', linewidth = 1.5, label='right hand trajectory')
+ax3.plot(x3, y3, z3, c='g', linewidth = 1.5, label='left hand trajectory')
 
 ax4.contourf(xi, yi, zi, zdir='z', cmap=cm.gist_rainbow)
 
-ax4.quiver(X,Y,Z,u,v,w, length=0.01, normalize=True)
+#ax4.quiver(X,Y,Z,u,v,w, length=0.01, normalize=True)
 
-ax.legend(fontsize= 15)
+ax.legend()
 ax.set_xlabel('X (m)', fontsize=10, fontweight='bold')
 ax.set_ylabel('Z (m)', fontsize=10, fontweight='bold')
 ax.set_zlabel('Y (m)', fontsize=10, fontweight='bold')
